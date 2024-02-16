8b63c7d3d904149c9c06b1f13ef2be60cd9963c8
+ <+>
2024-02-17 02:52:10 +0300

    adding routes

--- app/app.py
+++ app/app.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
-from models import Pizza, Restaurant, RestaurantPizza
+from models import Pizza, Restaurant, RestaurantPizza, db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 db = SQLAlchemy(app)
+
+class GetRestaurant(Resource):
+    pass
+    
+class GetRestaurantById(Resource):
+    pass
+
+
+if __name__ == "__main__":
+    app.run(port = 5555)
